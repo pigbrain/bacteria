7ed778a5c1661e9fe32412236e9626739482f3cf
pigbrain <hyundong.lee@nhnent.com>
2016-10-19 09:53:19 +0900

    add microscope skel

---
+++ microscope/src/main/java/bacteria/microscope/MicroscopeApplication.java
@@ -0,0 +1,12 @@
+package bacteria.microscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroscopeApplication.class, args);
+	}
+}
---
+++ microscope/src/main/java/bacteria/microscope/controller/HelloController.java
@@ -0,0 +1,13 @@
+package bacteria.microscope.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "helloworld!";
+	}
+}
